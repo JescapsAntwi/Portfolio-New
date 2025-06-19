95529bc31ce09d583c23c4a3ba9d2fa31f279a2a
JescapsAntwi <antwijescaps1@gmail.com>
2025-06-19 13:06:38 +0000

    Updated Projects List

--- components/projects.tsx
+++ components/projects.tsx
@@ -103,7 +103,7 @@ const projects = [
     image: "/projects/project-16.png",
     liveSite: "https://spin-to-survive.vercel.app/",
     github: "https://github.com/JescapsAntwi/spin-to-survive",
-    techStack: ["React", "CSS", "JavaScript"],
+    techStack: ["React", "TypeScript", "JavaScript"],
   },
   {
     title: "typing test game",
