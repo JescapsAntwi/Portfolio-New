958e6eb14f3e9ee62c2c3dfb8183cc003f19defc
JescapsAntwi <antwijescaps1@gmail.com>
2025-06-22 10:45:36 +0000

    Added new work experience

--- components/about.tsx
+++ components/about.tsx
@@ -231,7 +231,7 @@ const About = () => {
                 <div>
                   <div className="flex items-center gap-2 mb-4">
                     <h4 className="font-bold text-xl bg-gradient-to-r from-red-500 to-red-700 bg-clip-text text-transparent">
-                      Machine Learning Intern - Codveda Technologies
+                      Machine Learning Intern (Remote) - Codveda Technologies
                     </h4>
                     <span className="text-red-500">•</span>
                     <a
