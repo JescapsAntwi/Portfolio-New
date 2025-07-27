93de7dad1b437e3480ca549b648d662337b2cc88
JescapsAntwi <antwijescaps1@gmail.com>
2025-07-27 12:45:35 +0000

    added new work experience

--- components/about.tsx
+++ components/about.tsx
@@ -152,7 +152,7 @@ const About = () => {
                     </h4>
                     <span className="text-red-500">•</span>
                     <a
-                      href="https://github.com/JescapsAntwi/OIBSIP"
+                      href="https://github.com/JescapsAntwi/jpmorgan-chase---co"
                       target="_blank"
                       rel="noopener noreferrer"
                       className="text-red-500 hover:text-red-600 hover:underline"
